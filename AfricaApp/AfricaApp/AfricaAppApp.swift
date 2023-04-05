db2ed9a2b09fe540324c1fcc3d74cf969a32f03c
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-04-05 09:56:20 -0400

    AfricaApp initial commit

---
+++ AfricaApp/AfricaApp/AfricaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaAppApp.swift
+//  AfricaApp
+//
+//  Created by Thomas Cowern on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
