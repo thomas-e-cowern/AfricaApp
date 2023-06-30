9d613f4ca915947faef09d1c585a8d7f94633ed3
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-30 09:15:47 -0400

    add view folder and cover image view file

---
+++ AfricaApp/AfricaApp/View/CoverImageView.swift
@@ -0,0 +1,25 @@
+//
+//  CoverImageView.swift
+//  AfricaApp
+//
+//  Created by Thomas Cowern on 6/30/23.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    // MARK: Properties
+    
+    // MARK: Body
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: Preview
+struct CoverImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
+    }
+}
