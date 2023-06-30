bff60dcb528c4d24750b0ebd9ac13a1172c79277
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-30 09:53:48 -0400

    update codable extention

--- AfricaApp/AfricaApp/Extension/CodbleBundle+Extension.swift
+++ AfricaApp/AfricaApp/Extension/CodbleBundle+Extension.swift
@@ -12,16 +12,24 @@ extension Bundle {
     func decode(_ file: String) -> [CoverImage] {
         
         // Locate the json file
-        
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle")
+        }
         
         // create a property for the data
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle")
+        }
         
         // create decoder
+        let decoder = JSONDecoder()
         
         // create a property for the decoded data
+        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle")
+        }
         
         // return the data
-        
-        return []
+        return loaded
     }
 }
