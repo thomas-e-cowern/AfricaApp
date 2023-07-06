fc1f6310d79135aae2ce68ef166c98f8518f4b2b
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-06 09:35:37 -0400

    add inset fact view

---
+++ AfricaApp/AfricaApp/View/InsetFactView.swift
@@ -0,0 +1,35 @@
+//
+//  InsetFactView.swift
+//  AfricaApp
+//
+//  Created by Thomas Cowern on 7/6/23.
+//
+
+import SwiftUI
+
+struct InsetFactView: View {
+    // MARK: - Properties
+    let animal: Animal
+    
+    // MARK: - Body
+    var body: some View {
+        GroupBox {
+            TabView {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            } // MARK: Tabs
+            .tabViewStyle(PageTabViewStyle())
+            .frame(minHeight: 148, idealHeight: 168, maxHeight: 180)
+        } // MARK: End of group box
+    }
+}
+
+// MARK: - Preview
+struct InsetFactView_Previews: PreviewProvider {
+    static var previews: some View {
+        let animals: [Animal] = Bundle.main.decode("animals.json")
+        
+        InsetFactView(animal: animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
