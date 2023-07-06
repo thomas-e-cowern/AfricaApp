b278d879654666c04002ae9218ee4cd719d54e0d
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-06 11:06:22 -0400

    add video list item

---
+++ AfricaApp/AfricaApp/View/VideoListItem.swift
@@ -0,0 +1,57 @@
+//
+//  VideoListItem.swift
+//  AfricaApp
+//
+//  Created by Thomas Cowern on 7/6/23.
+//
+
+import SwiftUI
+
+struct VideoListItem: View {
+    
+    // MARK: - Properties
+    let video: Video
+    
+    // MARK: - Body
+    var body: some View {
+        HStack(spacing: 10) {
+            ZStack {
+                Image("video-\(video.id)")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 80)
+                    .cornerRadius(9)
+                
+                Image(systemName: "play.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 32)
+                    .shadow(radius: 4)
+            } //: End of ZStack
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text(video.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                
+                Text(video.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+            } //: End of VStack
+        } //: End of HStack
+    }
+}
+
+// MARK: - Preview
+struct VideoListItem_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        let videos: [Video] = Bundle.main.decode("videos.json")
+        
+        VideoListItem(video: videos[2])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
