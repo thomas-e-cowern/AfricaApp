fbee7262c88afe95aef0119cc5def50702ba4ec0
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-06 12:24:07 -0400

    update video list view with shuffle functionality

--- AfricaApp/AfricaApp/App/VideoListView.swift
+++ AfricaApp/AfricaApp/App/VideoListView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct VideoListView: View {
     
     // MARK: - Properties
-    var videos: [Video] = Bundle.main.decode("videos.json")
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
     
     // MARK: - Body
     var body: some View {
@@ -24,6 +24,16 @@ struct VideoListView: View {
             .listStyle(InsetGroupedListStyle())
             .navigationTitle("Videos")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        videos.shuffle()
+                    } label: {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+
+                }
+            }
         } // MARK: End of Navigation
     }
 }
