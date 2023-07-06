6fe6c221f8ebc35acb781a92d2f92d8827c9740b
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-06 10:56:19 -0400

    add video model to models

---
+++ AfricaApp/AfricaApp/Model/VideoModel.swift
@@ -0,0 +1,14 @@
+//
+//  VideoModel.swift
+//  AfricaApp
+//
+//  Created by Thomas Cowern on 7/6/23.
+//
+
+import Foundation
+
+struct Video: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+}
