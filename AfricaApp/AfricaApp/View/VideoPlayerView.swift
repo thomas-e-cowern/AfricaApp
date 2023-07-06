b38d1207a6f7a57d102f5541ff94740ae606c49f
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-06 12:56:47 -0400

    update video player view formatting and title

--- AfricaApp/AfricaApp/View/VideoPlayerView.swift
+++ AfricaApp/AfricaApp/View/VideoPlayerView.swift
@@ -10,18 +10,38 @@ import AVKit
 
 struct VideoPlayerView: View {
     // MARK: - Properties
+    var videoSelected: String
+    var videoTitle :String
     
     // MARK: - Body
     var body: some View {
-        VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: "cheetah", withExtension: "mp4")!))
+        VStack {
+            VideoPlayer(player: playVideo(filename: videoSelected, fileFormat: "mp4")) {
+//                Text(videoTitle)
+            }
+            .overlay(
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32)
+                    .padding(.top, 6)
+                    .padding(.horizontal, 8)
+                , alignment: .topLeading
+            )
+        } //: End of VStack
+        .tint(.accentColor)
+        .navigationTitle(videoTitle)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 // MARK: - Preview
 struct VideoPlayerView_Previews: PreviewProvider {
     static var previews: some View {
-        VideoPlayerView()
-            .previewLayout(.sizeThatFits)
-            .padding()
+        NavigationView {
+            VideoPlayerView(videoSelected: "lion", videoTitle: "Lion")
+                .previewLayout(.sizeThatFits)
+                .padding()
+        }
     }
 }
