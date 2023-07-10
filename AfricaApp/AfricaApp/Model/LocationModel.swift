2172479a9e25a3510c561bb22e6069e05e41f772
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-10 12:06:40 -0400

    add location model

---
+++ AfricaApp/AfricaApp/Model/LocationModel.swift
@@ -0,0 +1,18 @@
+//
+//  LocationModel.swift
+//  AfricaApp
+//
+//  Created by Thomas Cowern on 7/10/23.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+}
+
