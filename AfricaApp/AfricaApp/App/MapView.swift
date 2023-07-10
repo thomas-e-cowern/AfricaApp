7f1e7684f6b0973fc92194be232c030f57944071
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-10 12:16:53 -0400

    add region computed property to map view

--- AfricaApp/AfricaApp/App/MapView.swift
+++ AfricaApp/AfricaApp/App/MapView.swift
@@ -10,6 +10,13 @@ import MapKit
 
 struct MapView: View {
     // MARK: - Properties
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.00286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        
+        return mapRegion
+    }()
     
     // MARK: - Body
     var body: some View {
