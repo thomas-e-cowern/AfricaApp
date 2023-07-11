3595b79d4e20f3811f8f045a6360692da4cfbc9a
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-11 08:35:34 -0400

    update animation in map annotation view

--- AfricaApp/AfricaApp/View/MapAnnotationView.swift
+++ AfricaApp/AfricaApp/View/MapAnnotationView.swift
@@ -11,6 +11,8 @@ struct MapAnnotationView: View {
     // MARK: - Properties
     var location: NationalParkLocation
     
+    @State private var animation: Double = 0.0
+    
     // MARK: - Body
     var body: some View {
         ZStack {
@@ -21,6 +23,8 @@ struct MapAnnotationView: View {
             Circle()
                 .stroke(Color.accentColor, lineWidth: 2)
                 .frame(width: 52, height: 52)
+                .scaleEffect(1 + CGFloat(animation))
+                .opacity(1 - animation)
             
             Image(location.image)
                 .resizable()
@@ -28,6 +32,11 @@ struct MapAnnotationView: View {
                 .frame(width: 48, height: 48)
                 .clipShape(Circle())
         } //: End of ZStack
+        .onAppear {
+            withAnimation(Animation.easeOut(duration: 2).repeatForever(autoreverses: false)) {
+                animation = 1
+            }
+        }
     }
 }
 
