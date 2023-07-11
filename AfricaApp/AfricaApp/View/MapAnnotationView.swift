a663791f5b943f744d9403b8728cc23cf270921b
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-11 08:26:15 -0400

    add map annotation view

---
+++ AfricaApp/AfricaApp/View/MapAnnotationView.swift
@@ -0,0 +1,26 @@
+//
+//  MapAnnotationView.swift
+//  AfricaApp
+//
+//  Created by Thomas Cowern on 7/11/23.
+//
+
+import SwiftUI
+
+struct MapAnnotationView: View {
+    // MARK: - Properties
+    
+    // MARK: - Body
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - Preview
+struct MapAnnotationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapAnnotationView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
