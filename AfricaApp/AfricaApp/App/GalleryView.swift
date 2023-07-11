9d25b7f46b04ef229b1f767a0518b098f2cf3883
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-11 10:00:43 -0400

    update gallery view with grid layout, and repeating images

--- AfricaApp/AfricaApp/App/GalleryView.swift
+++ AfricaApp/AfricaApp/App/GalleryView.swift
@@ -8,15 +8,40 @@
 import SwiftUI
 
 struct GalleryView: View {
+    // MARK: - Properties
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    // Simple grid definition
+    //let gridLayout: [GridItem] = [
+    //    GridItem(.flexible()),
+    //    GridItem(.flexible()),
+    //    GridItem(.flexible())
+    //]
+    
+    // Efficient grid definition
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    // MARK: - Body
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            
+            // Grid
+            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                ForEach(animals) { item in
+                    Image(item.image)
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                } //: End of ForEach
+            } //: End of grid
         }  //: End of ScrollView
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
     }
 }
 
+// MARK: - Preview
 struct GalleryView_Previews: PreviewProvider {
     static var previews: some View {
         GalleryView()
